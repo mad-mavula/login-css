295cdfa55e68abf58ba749baf8c2a3257aff7c5a
mad-mavula <59197160+mad-mavula@users.noreply.github.com>
2020-01-05 08:09:21 +0530

    fotm updated

---
+++ Popup/floatjs.js
@@ -0,0 +1,7 @@
+document.getElementById('login').addEventListener('click', function(){
+	document.querySelector('.float-bg').style.display='flex';
+});
+
+document.querySelector('.exit').addEventListener('click', function(){
+	document.querySelector('.float-bg').style.display='none';
+});
